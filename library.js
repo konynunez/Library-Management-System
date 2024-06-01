d81679b1c52ea650e406016a55a828da4b55aef7
Kony <konynunez@gmail.com>
2024-06-01 12:52:21 -0400

    testing part

--- library.js
+++ library.js
@@ -46,35 +46,46 @@ class Book {
     }
   }
 
-  // Testing
-  let library = new Library("My Library");
+  // Initialize Library
+let library = new Library("My Library");
 
-  let book1 = new Book("The Fellowship of the Ring", "John Ronald Tolkien", "9788496208490", 5);
-  let book2 = new Book("A Game of Thrones (A Song of Ice and Fire, #1", "George R. R. Martin", "9781101965870", 3);
-  let book3 = new Book("The Name of the Wind", "Patrick Roth-fuss", "9780756404741", 3);
+// Create Books
+let book1 = new Book("The Fellowship of the Ring", "John Ronald Tolkien", "9788496208490", 5);
+let book2 = new Book("A Game of Thrones (A Song of Ice and Fire, #1)", "George R. R. Martin", "9781101965870", 3);
+let book3 = new Book("The Name of the Wind", "Patrick Rothfuss", "9780756404741", 3);
+let book4 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "9780747532699", 4);
 
-  library.addBook(book1);
-  library.addBook(book2);
-  library.addBook(book3);
- 
+// Add books
+library.addBook(book1);
+library.addBook(book2);
+library.addBook(book3);
+library.addBook(book4);
 
-  library.listAllBooks();
+//remove book
+let isbnToRemove = "9788496208490"; 
+library.removeBook(9780756404741);
 
-  let foundBook = library.findBookByTitle("The Fellowship of the Ring");
-  console.log(`Found Book: ${foundBook.title}`);
+//testing
+console.log("Updated Library:");
+console.log(library);
 
-  library.removeBook("9780756404741");
-  library.listAllBooks();
+console.log("Methods in Book class:");
+console.log(Object.getOwnPropertyNames(Book.prototype));
 
-  console.log(library)
-  // Log the prototype of the Book class
-  console.log("Methods in Book class:");
-  console.log(Object.getOwnPropertyNames(Book.prototype));
-  
-  // Log the prototype of the Library class
-  console.log("Methods in Library class:");
-  console.log(Object.getOwnPropertyNames(Library.prototype));
-  // Log the entire content of the library object
-  console.log("Library object:");
-  console.log(JSON.stringify(library, null, 2));
+console.log("Methods in Library class:");
+console.log(Object.getOwnPropertyNames(Library.prototype));
 
+console.log("Borrowing a book...");
+book1.borrowBook();
+console.log(`Available copies of "${book1.title}": ${book1.availableCopies}`);
+
+console.log("Returning a book...");
+book1.returnBook();
+console.log(`Available copies of "${book1.title}": ${book1.availableCopies}`);
+
+console.log("Finding a book by title...");
+let foundBook = library.findBookByTitle("The Fellowship of the Ring");
+console.log(`Found book: ${foundBook.title}`);
+
+console.log("Listing all books...");
+library.listAllBooks();
